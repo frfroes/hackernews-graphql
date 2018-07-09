7afc9c6d57363557a843ee333c84000b7cc187dd
Filipe Fróes <filipe.froes@ssp.ba.gov.br>
2018-07-09 12:52:05 -0300

    Schema separation into its own file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 import { GraphQLServer } from "graphql-yoga";
 
-const typeDefs = `
-type Query {
-    info: String!
-    feed: [Link!]!
-}
-
-type Link {
-    id: ID!
-    description: String!
-    url: String!
-}
-`
 let links = [{
     id: 'link-0',
     url: 'www.howtographql.com',
@@ -26,7 +14,7 @@ const resolvers = {
 }
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers
 })
 
