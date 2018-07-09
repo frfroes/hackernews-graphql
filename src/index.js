d14d7c5c33347b9923d597b08e3832c076145f86
Filipe Fróes <filipe.froes@ssp.ba.gov.br>
2018-07-09 12:50:15 -0300

    Implementation of a simple feed query which fetchs in memory data

--- src/index.js
+++ src/index.js
@@ -3,12 +3,26 @@ import { GraphQLServer } from "graphql-yoga";
 const typeDefs = `
 type Query {
     info: String!
+    feed: [Link!]!
+}
+
+type Link {
+    id: ID!
+    description: String!
+    url: String!
 }
 `
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}]
+
 const resolvers = {
     Query: {
-        info: () => 'This is the API of a Hackernews Replica'
-    }
+        info: () => 'This is the API of a Hackernews Replica',
+        feed: () => links,
+    },
 }
 
 const server = new GraphQLServer({
